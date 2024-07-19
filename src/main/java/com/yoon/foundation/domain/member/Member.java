6445a59a72e1e4556a832e4dc4fe96cd110d894a
jeontaehyeon <jth660084@gmail.com>
2024-07-20 03:39:48 +0900

    안녕하세요

---
+++ src/main/java/com/yoon/foundation/domain/member/Member.java
@@ -0,0 +1,37 @@
+package com.yoon.foundation.domain.member;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="TB_MEMBER")
+@Getter
+@Setter
+
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private Integer satiety;
+
+    @Column(name ="service_size")
+    private double servingSize;
+
+    @Column(name = "carbon_pledge")
+    private double carbonPledge;
+
+    public Member(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
